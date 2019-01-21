573f778aa49217f33ca03f06c916694214525eb6
Rafal Jeczalik <rjeczalik@gmail.com>
2019-01-21 15:30:23 +0100

    write: use all nodes

--- scripts/write.sh
+++ scripts/write.sh
@@ -6,5 +6,5 @@ cassandra-stress write \
 	cl=QUORUM n=${count} \
 	-mode native cql3 user="${username}" password="${password}" \
 	-rate threads=350 limit='${limit}/s' \
-	-node "${first_seed}" \
+	-node "${seeds}" \
 	-pop seq=${range_from}..${range_to}
